cdb68c7843460611694428c38f007f5c67522c8d
SparkZHarrY <66181850+SparkZHarrY@users.noreply.github.com>
2023-12-28 22:26:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "847244116"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "postgres://bsixntcb:vrVSnJqS_HoIsqfLxyTWSplaUOsMaRVO@john.db.elephantsql.com/bsixntcb")
-DB_NAME = os.environ.get("DATABASE_NAME", "filez")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sparkzharry007:@v$HzuEaFY4f@cluster0.0pz61tn.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
